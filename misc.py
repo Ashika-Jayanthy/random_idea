4448e55af5e65683be634c0f3e9708ca54b20c26
Ashika Jayanthy <jayanthyashika@gmail.com>
2022-11-15 10:26:23 +0530

    .

--- misc.py
+++ misc.py
@@ -1,9 +1,11 @@
 import numpy as np
-import sys
 
 def prime_upper(n):
     return n*np.log(n)
 
+# options: 1) represent by roots + relationship between roots (over different rings). are galois groups fundamental? if not,
+# find a different representation
+# 2) something else  <- keep thinking.
 
 class PolynomialRepresentations():
     def __init__(self,input_graph, degree=1, ring_type="Z", representation_type=''):
@@ -11,9 +13,8 @@ class PolynomialRepresentations():
         self.ring_type = ring_type
         self.representation_type = representation_type
 
-
     def compute_representation_from_graph(self):
-        sys.(run_command)( )
+
         return representation
 
 class RandomGraphs():
@@ -21,3 +22,9 @@ class RandomGraphs():
         self.average_degree = average_degree
         self.n_nodes = n_nodes
         self.n_edges = n_edges
+
+    def generate_random_graph(self):
+        return
+
+    def polynomial_from_graph(self):
+        return
