319f94c480d269abad189d4779799a047827f17a
Ashika Jayanthy <jayanthyashika@gmail.com>
2022-11-15 09:25:35 +0530

    .

---
+++ misc.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sys
+
+def prime_upper(n):
+    return n*np.log(n)
+
+
+class PolynomialRepresentations():
+    def __init__(self,input_graph, degree=1, ring_type="Z", representation_type=''):
+        self.input_graph = input_graph
+        self.ring_type = ring_type
+        self.representation_type = representation_type
+
+
+    def compute_representation_from_graph(self):
+        sys.(run_command)( )
+        return representation
+
+class RandomGraphs():
+    def __init__(self,average_degree,n_nodes,n_edges):
+        self.average_degree = average_degree
+        self.n_nodes = n_nodes
+        self.n_edges = n_edges
