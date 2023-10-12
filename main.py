c2c57cfce4df5d84bfe3843c3350af3d1b653d06
Ashika Jayanthy <jayanthyashika@gmail.com>
2023-10-12 14:25:59 +0530

    corrections

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from scipy.linalg import expm
 from scipy.special import bernoulli
 from scipy.special import gamma
 from scipy.special import zeta
+import math
 
-
-####
-
+def prime_upper(n):
+    return n*np.log(n)
 
 def reimann_functional(z):
     return (2**z * np.pi**z-1 * np.sin(np.pi*z/2.) * gamma(1-z) * zeta(1-z))
@@ -18,68 +18,61 @@ def coprime_probability(set_of_values):
     # euler's formula
     return 1./zeta(len(set_of_values))
 
-def chinese_remainder_solve():
-    return
+def extended_gcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, x, y = extended_gcd(b % a, a)
+        return (g, y - (b // a) * x, x)
 
-icomplex = complex(0,1)
+def mod_inverse(a, m):
+    g, x, _ = extended_gcd(a, m)
+    if g != 1:
+        raise ValueError("Modular inverse does not exist")
+    else:
+        return x % m
 
-ket_zero = np.array([1,0])
-ket_one = np.array([0,1])
-
-rotation_generators = 0.5 * np.array([
-[[0.,1.],[1.,0.]],
-[[0.,-icomplex],[icomplex,0.]],
-[[1.,0.],[0.,-1.]]
-])
+def chinese_remainder_theorem(n, a):
+    prod = math.prod(n)
+    result = 0
+    for n_i, a_i in zip(n, a):
+        m = prod // n_i
+        m_inv = mod_inverse(m, n_i)
+        result += a_i * m * m_inv
+    
+    return result % prod
 
 
 def heaviside_smooth(x,k=1):
     # logistic
     return 1. / 1. + np.exp(-2*k*x)
 
-def matrix_multiply(a,b):
-    n1,m1 = a.shape
-    n2,m2 = b.shape
-    ans = np.zeros((n1,m2),dtype="complex128")
-    for row in range(n1):
-        row_value = a[row]
-        for column in range(m2):
-            column_value = b.T[column]
-            ans[row,column] = np.vdot(row_value,column_value)
-    return ans
-
-def commutator(a,b):
-    return matrix_multiply(a,b) - matrix_multiply(b,a)
-
-def coprime_probability(set_of_values):
-    # euler's formula
-    return 1./zeta(len(set_of_values))
+def rho_function(weights, dims,quantum_perceptron_type):
+    if quantum_perceptron_type == "Ising":
+        y = np.multiply(np.heaviside(weights - 1) + np.heaviside(weights + 1), axis=1)
+    elif quantum_perceptron_type == "Spherical":
+        y = np.heaviside(np.absolute(weights) **2 - dims)
+    return  y / np.sum(y)
 
 
 class Perceptron():
-    def __init__(self, input_vector, expectation, quantum_perceptron_type = "Spherical"):
-        assert type(input_vector == np.complex64)
+    def __init__(self, input_vector, expectation, perceptron_type = "Spherical"):
+        assert type(input_vector) == np.complex64
         assert input_vector.shape[0] == 2
-        assert np.isclose(np.absolute(self.input_vector[:,0])**2, np.absolute(np.input_vector[:,1])**2,1.)
+        assert np.isclose(np.absolute(input_vector[:,0])**2, np.absolute(input_vector[:,1])**2,1.)
         self.input_vector = input_vector
-        self.hilbert_space_dim = self.input_vector.shape[1]
+        self.space_dim = self.input_vector.shape[1]
         self.threshold = np.zeros(self.input_vector.shape[1])
         self.expectation = expectation
-        # check axes
-        def rho_function(weights, dims):
-            if quantum_perceptron_type == "Ising":
-                y = np.multiply(np.heaviside(weights - 1) + np.heaviside(weights + 1), axis=1)
-            elif quantum_perceptron_type == "Spherical":
-                y = np.heaviside(np.absolute(weights) **2 - dims)
-            return  y / np.sum(y)
-
+        self.perceptron_type = perceptron_type
+        
     def learn_weights(self):
-        self.weights = MaxEntropy(self.input_vector,self.expectation)
+        self.weights = MaxEntropy(self.input_vector,self.expectation) # placeholder
         return
 
     def calculate_volume(self):
         # wrong
-        self.perceptron_volume = np.sum(np.multiply(heaviside_smooth(self.X))*rho_function(self.weights))
+        self.perceptron_volume = np.sum(np.multiply(heaviside_smooth(self.X))*rho_function(self.weights, self.space_dim, self.perceptron_type))
         return
 
     def run_all(self):
