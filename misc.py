b881ec9549dd417b61619b189629d227b800467f
Ashika Jayanthy <jayanthyashika@gmail.com>
2023-10-12 14:13:03 +0530

    some corrections

--- misc.py
+++ misc.py
@@ -1,17 +1,28 @@
 import numpy as np
-
-def prime_upper(n):
-    return n*np.log(n)
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 class PolynomialRepresentations():
-    def __init__(self,input_graph, degree=1, ring_type="Z", representation_type=''):
+    def __init__(self, input_graph, degree=1):
         self.input_graph = input_graph
-        self.ring_type = ring_type
-        self.representation_type = representation_type
+        self.degree = degree
+        self.representation = None
 
     def compute_representation_from_graph(self):
+        X = self.input_graph['nodes'] # placeholder
+        y = self.input_graph['edges'] # placeholder
+        
+        poly_features = PolynomialFeatures(degree=self.degree)
+        X_poly = poly_features.fit_transform(X)
 
-        return representation
+        model = LinearRegression()
+        model.fit(X_poly, y)
+
+        coefficients = model.coef_
+
+        self.representation = coefficients
+        return self.representation
+
 
 class Graphs():
     def __init__(self, nodes, edges):
@@ -19,7 +30,7 @@ class Graphs():
         self.edges = edges
         self.G = {}
 
-    def generate_graph(self, values):
-        for i in range(len(self.n_nodes)):
-            self.G[nodes[i]] = self.edges[i]
+    def generate_graph(self):
+        for i in range(len(self.nodes)):
+            self.G[self.nodes[i]] = self.edges[i]
         return self.G
