d14193212a3e8a44abfb2c90ec21924d7725ad29
Ashika Jayanthy <jayanthyashika@gmail.com>
2023-10-12 13:53:54 +0530

    .

--- misc.py
+++ misc.py
@@ -3,11 +3,6 @@ import numpy as np
 def prime_upper(n):
     return n*np.log(n)
 
-# options: 1) represent by roots + relationship between roots (over different rings). are galois groups fundamental? if not,
-# find a different representation
-# 2) something else  <- keep thinking.
-# 2a) how to order sequence of polynomials. with index set primes ordered by size less than n; or for number fields prime ideals (how to order?).
-
 class PolynomialRepresentations():
     def __init__(self,input_graph, degree=1, ring_type="Z", representation_type=''):
         self.input_graph = input_graph
